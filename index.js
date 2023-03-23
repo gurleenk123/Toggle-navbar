d12c5e439c1fb2ac74072c9cddd7fa24eb785b16
Gurleen Kaur <56589123+gurleenk123@users.noreply.github.com>
2023-03-23 18:02:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function classToggle() {
+    const navs = document.querySelectorAll('.Navbar__Items')
+    
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  }
+  
+  document.querySelector('.Link-toggle')
+    .addEventListener('click', classToggle);
